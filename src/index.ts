b2b2b7f7a498c2dea51e1996fe6dac2abfb09d46
cmack11 <claymackenthun@yahoo.com>
2025-02-24 18:51:02 -0600

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
-import { GpioMapping, LedMatrix, LedMatrixUtils, PixelMapperType } from 'rpi-led-matrix';
+import {
+  GpioMapping,
+  LedMatrix,
+  LedMatrixUtils,
+  PixelMapperType,
+} from 'rpi-led-matrix';
 
-const matrix = new LedMatrix ({
+const matrix = new LedMatrix(
+  {
     ...LedMatrix.defaultMatrixOptions(),
     rows: 32,
     cols: 64,
-    chainLength: 2, 
+    chainLength: 2,
     hardwareMapping: GpioMapping.AdafruitHat,
     pixelMapperConfig: LedMatrixUtils.encodeMappers({
-        type: PixelMapperType.U
-    })
-}, {
+      type: PixelMapperType.U,
+    }),
+  },
+  {
     ...LedMatrix.defaultRuntimeOptions(),
-    gpioSlowdown: 1
-})
+    gpioSlowdown: 1,
+  },
+);
 
-matrix
-    .clear()
-    .brightness(100)
-    .fgColor(0x0000ff)
-    .fill()
-    .sync();
+matrix.clear().brightness(100).fgColor(0x0000ff).fill().sync();
 
 // Exit after 5 seconds (5000 milliseconds)
 setTimeout(() => {
-    console.log('Exiting');
-    matrix.clear().sync();
-    process.exit(0); // 0 indicates successful exit
-  }, 5000);
+  console.log('Exiting');
+  matrix.clear().sync();
+  process.exit(0); // 0 indicates successful exit
+}, 5000);
